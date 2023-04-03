8c7a9e980f1aa583f4365d30ab292dc46c71a616
Manu <manuel.brusche@web.de>
2023-04-03 21:13:18 +0200

    added dto structure to /{id} endpoint

--- src/main/java/com/manu/BergfexScraper/repository/SkiResortRepository.java
+++ src/main/java/com/manu/BergfexScraper/repository/SkiResortRepository.java
@@ -1,9 +1,13 @@
 package com.manu.BergfexScraper.repository;
 
+import com.manu.BergfexScraper.dto.SingleResortResponseDTO;
+import com.manu.BergfexScraper.dto.SkiResortAndAllTimelinesDTO;
 import com.manu.BergfexScraper.dto.SkiResortAndTimelineDTO;
 import com.manu.BergfexScraper.model.SkiResort;
+import com.manu.BergfexScraper.model.SkiResortTimeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.net.URL;
 import java.util.Date;
@@ -29,6 +33,26 @@ public interface SkiResortRepository extends JpaRepository<SkiResort, Long> {
             "WHERE t.date = (SELECT MAX(t2.date) FROM SkiResortTimeline t2 WHERE t2.skiResort = r) " +
             "ORDER BY t.snowHeightMountain DESC")
     List<SkiResortAndTimelineDTO> findAllResortsWithTimelineOrderedBySnowMountainHeight();
+
+    @Query("SELECT new com.manu.BergfexScraper.dto.SkiResortAndTimelineDTO(" +
+            "r.Id, r.name, t.date, t.snowHeightMountain, t.snowHeightValley, t.liftsOpen, t.pistesOpen) " +
+            "FROM SkiResort r LEFT JOIN SkiResortTimeline t " +
+            "ON r.Id = t.skiResort.Id " +
+            "where t.date = (SELECT MAX(t2.date) FROM SkiResortTimeline t2 WHERE t2.skiResort = r) AND " +
+            "r.Id = :id")
+    SkiResortAndTimelineDTO findResortWithTimeline(@Param("id") Long id);
+
+    @Query("SELECT new com.manu.BergfexScraper.dto.SkiResortAndTimelineDTO(" +
+            "r.Id, r.name, t.date, t.snowHeightMountain, t.snowHeightValley, t.liftsOpen, t.pistesOpen) " +
+            "FROM SkiResort r LEFT JOIN SkiResortTimeline t " +
+            "ON r.Id = t.skiResort.Id " +
+            "where t.date = (SELECT MAX(t2.date) FROM SkiResortTimeline t2 WHERE t2.skiResort = r) AND " +
+            "r.Id = :id")
+    SkiResortAndAllTimelinesDTO findResortWithAllTimelines(Long id);
+
+
+
+//    (Long Id, String skiResortName, Date updatedOn, Integer showHeightMountain, Integer showHeightValley, String liftsOpen, String pistesOpen
 
 //    private Long Id;
 //    private String skiResortName;
